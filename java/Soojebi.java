c96b1760d47938e0acc0f1196f1a74577afdc410
maxkim7723@gmail.com <maxkim7723@gmail.com>
2024-07-02 14:42:28 +0900

    update

---
+++ java/Soojebi.java
@@ -0,0 +1,25 @@
+public class Soojebi {
+    int a = 5;
+    void fn(){
+        a = a + 3;
+    }
+    public static void main(String[] args) {
+        Soojebi s = new Soojebi();
+        s.a = s.a + 5;
+        s.fn();
+        System.out.println(s.a);
+    }
+}
+
+public class Soojebi {
+    int a = 5;
+    void fn(){
+        a = a + 3;
+    }
+    public static void main(String[] args) {
+        Soojebi s = new Soojebi();
+        s.a = s.a + 5;
+        s.fn();
+        System.out.println(s.a);
+    }
+}
